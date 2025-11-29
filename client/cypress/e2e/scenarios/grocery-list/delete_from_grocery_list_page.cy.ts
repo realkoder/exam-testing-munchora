fc62e0f864e0be150d54ed5365435e62b04b11ad
realkoder <alexanderbtcc@gmail.com>
2025-11-29 20:15:18 +0100

    fixup! trying recursive approach

--- client/cypress/e2e/scenarios/grocery-list/delete_from_grocery_list_page.cy.ts
+++ client/cypress/e2e/scenarios/grocery-list/delete_from_grocery_list_page.cy.ts
@@ -1,13 +1,14 @@
-import { deleteListIfExist } from '../../../support/test_utils.cy';
+import {deleteListIfExist} from '../../../support/test_utils.cy';
 
 describe('Grocery List - Delete', () => {
   beforeEach(() => {
     cy.loginOrSignUpByApi();
     cy.loadPage('groceryLists');
-    cy.contains('button', 'Create Your First List').click();
   });
 
   it('deletes grocery list successfully', () => {
+    cy.contains('button', 'Create Your First List').click();
+    cy.get('input[placeholder="Name Of Shopping List"]').should('have.value', 'Shopping 🛒');
     deleteListIfExist();
     cy.contains('h3', 'No grocery lists yet').should('be.visible');
   });
