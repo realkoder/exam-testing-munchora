2bea474987e6f88e0d20274d2148a919f6cf4359
realkoder <alexanderbtcc@gmail.com>
2025-11-29 20:36:11 +0100

    fixup! fixup! fixup! fixup! trying recursive approach

--- client/cypress/support/test_utils.cy.ts
+++ client/cypress/support/test_utils.cy.ts
@@ -3,13 +3,14 @@ export function deleteListIfExist() {
     const deleteBtns = doc.querySelectorAll('button[cy-data="delete-list-btn"]');
 
     if (deleteBtns.length) {
-      cy.wrap(deleteBtns[0]).click({force: true});
+      btn = deleteBtns[0]
+      cy.wrap(btn).click({force: true});
 
       cy.get('[role="dialog"]').should('be.visible').within(() => {
         cy.contains('button', 'Delete').click({force: true});
       });
 
-      cy.wrap(deleteBtns[0]).should('not.exist').then(() => {
+      cy.wrap(btn).should('not.exist').then(() => {
         if (deleteBtns.length > 1) deleteListIfExist();
       });
     }
