ea0327f389d975eb4d1cdd8c58090a9bfef7f046
realkoder <alexanderbtcc@gmail.com>
2025-12-04 18:20:03 +0100

    fixup! fixup! grocerylist api tests. small fixes and cleans.

--- munchora/server/app/services/grocery_lists/sharer.rb
+++ munchora/server/app/services/grocery_lists/sharer.rb
@@ -1,12 +1,10 @@
-module GroceryLists
-  class Sharer
-    def self.share(list, user_ids)
-      users_to_add = User.where(id: user_ids).where.not(id: list.shared_user_ids)
-      list.shared_users << users_to_add
-    end
+class GroceryLists::Sharer
+  def self.share(list, user_ids)
+    users_to_add = User.where(id: user_ids).where.not(id: list.shared_user_ids)
+    list.shared_users << users_to_add
+  end
 
-    def self.unshare(list, user_id)
-      list.shared_users.delete(user_id)
-    end
+  def self.unshare(list, user_id)
+    list.shared_users.delete(user_id)
   end
 end
